6abe108d60446ac868f123bdb7ca67d4beb4ab35
Nate <me@n8m.us>
2024-12-04 12:44:50 -0800

    fix: enable redundant host IPs for local clusters

--- nodes/renterd.go
+++ nodes/renterd.go
@@ -258,6 +258,8 @@ func (m *Manager) StartRenterd(ctx context.Context, sk types.PrivateKey, ready c
 	}
 
 	ap, err := autopilot.New(config.Autopilot{
+		Enabled:                        true,
+		AllowRedundantHostIPs:          true,
 		Heartbeat:                      time.Second,
 		MigrationHealthCutoff:          0.99,
 		MigratorParallelSlabsPerWorker: 1,
@@ -277,7 +279,8 @@ func (m *Manager) StartRenterd(ctx context.Context, sk types.PrivateKey, ready c
 	node.Password = "sia is cool"
 
 	err = busClient.UpdateAutopilotConfig(ctx, func(req *api.UpdateAutopilotRequest) {
-		req.Enabled = &[]bool{true}[0]
+		enabled := true
+		req.Enabled = &enabled
 		req.Contracts = &api.ContractsConfig{
 			Amount:      1000,
 			Period:      4320,
