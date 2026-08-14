6e330a0afa4736e9f52e323dfdae910d94012cc8
Chris Schinnerl <3903476+ChrisSchinnerl@users.noreply.github.com>
2026-08-14 10:04:48 +0200

    fix build

--- nodes/walletd.go
+++ nodes/walletd.go
@@ -85,12 +85,12 @@ func (m *Manager) StartWalletd(ctx context.Context, ready chan<- struct{}) (err 
 			return fmt.Errorf("failed to open bolt db: %w", err)
 		}
 		defer bdb.Close()
-		dbstore, tipState, err := chain.NewDBStore(bdb, network, genesis, nil)
+		dbstore, err := chain.NewDBStore(bdb, network, genesis, nil)
 		if err != nil {
 			return fmt.Errorf("failed to create dbstore: %w", err)
 		}
 
-		cm = chain.NewManager(dbstore, tipState)
+		cm = chain.NewManager(dbstore)
 	}
 
 	syncerListener, err := net.Listen("tcp", ":0")
